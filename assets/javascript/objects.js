a3a56b9720bdbad8f09438bb4413265c91041ad5
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-10-27 23:24:54 -0500

    Separate the js files for better reading

---
+++ assets/javascript/objects.js
@@ -0,0 +1,36 @@
+// define character objects
+// darth vader object
+var darthVader = {
+  health: 80,
+  attack: 80,
+  counterAttack: 40,
+  image: "assets/images/darth_vader.jpg",
+  name: "Darth Vader"
+};
+
+// luke skywalker object
+var lukeSkywalker = {
+  health: 125,
+  attack: 40,
+  counterAttack: 20,
+  image: "assets/images/luke_skywalker.jpg",
+  name: "Luke Skywalker"
+};
+
+// obi-wan kenobi object
+var obiWanKenobi = {
+  health: 200,
+  attack: 20,
+  counterAttack: 10,
+  image: "assets/images/obi_wan.jpg",
+  name: "Obi-wan Kenobi"
+};
+
+// emperor palpatine object
+var emperorPalpatine = {
+  health: 100,
+  attack: 60,
+  counterAttack: 30,
+  image: "assets/images/emperor_palpatine.jpg",
+  name: "Emperor Palpatine"
+};
